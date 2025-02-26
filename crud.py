44e1047cf30a524c81d9e05218335a0baf7bb95c
CaioBattisti <cluizbattisti@gmail.com>
2025-02-26 19:21:59 -0300

    save

---
+++ crud.py
@@ -0,0 +1,13 @@
+import mysql.connector
+from config import MYSQL_HOST,MYSQL_USER,MYSQL_PASSWORD,MYSQL_DATABASE
+
+def get_connection():
+    return mysql.connector.connect(
+        host = MYSQL_HOST,
+        user = MYSQL_USER,
+        password = MYSQL_PASSWORD,
+        database = MYSQL_DATABASE)
+def create_user(nome, telefone,email,usuario,senha):
+    conn = get_connection()
+    corsor = conn.cursor()
+    query = "insert usuario"
