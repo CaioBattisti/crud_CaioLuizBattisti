b92ce73d4238e2b2152db1761432a6b5265c011b
CaioBattisti <cluizbattisti@gmail.com>
2025-02-26 20:38:48 -0300

    save

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ MYSQL_PASSWORD = 'root'
 MYSQL_DATABASE = 'caiobattisti_db'
 
 #para rodar o projeto nao posso esquecer de rodar na tela de console
-#pip install mysql.connector.pyton
+#pip install mysql-connector-pyton
